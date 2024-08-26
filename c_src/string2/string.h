ff2edbd4f43a656cd3ea042aacd78b4e2ba31e0a
윤소연 <yunsy616@gmail.com>
2024-08-26 09:00:24 +0900

    Add files via upload

---
+++ c_src/string2/string.h
@@ -0,0 +1,9 @@
+#ifndef STRING_H
+#define STRING_H
+
+/*unsigned*/int my_strlen(const char *s);
+void my_strcpy(char *des, const char *src);
+int my_strcmp(char *s1, const char *s2);
+void my_strcat(char *des, const char *src);
+
+#endif
